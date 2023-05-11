f2cb20b1233259b1fe0a49cf207934212491a912
Diogo Bruno <diogobruno07@gmail.com>
2023-05-10 22:39:28 -0300

    Add files via upload

---
+++ leitura_cadastro.R
@@ -0,0 +1,21 @@
+#limpa o ambiente
+rm(list=ls())
+
+library(readr)
+cadastro <- read_csv("C:/Users/logonrmlocal/Downloads/cadastro.csv")
+View(cadastro)
+
+# verifica os tipos
+str(cadastro)
+
+#analise descritiva
+#medidas resumo
+summary(cadastro)
+
+#mudar tipo da variavel para qualitativa
+cadastro$CEP <- factor(cadastro$CEP)
+
+str(cadastro$CEP)
+
+#mostrando a contagem de CEP
+summary(cadastro$CEP)
